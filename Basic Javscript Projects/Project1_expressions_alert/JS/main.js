8540be1b5b4982c17d8ab422b4ddd85a4d4b2625
Morganallard <morgan.allard15@gmail.com>
2023-01-01 14:24:41 -0800

    add javascript

---
+++ Basic Javscript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,19 @@
+
+var A = "Hello, World!"
+window.alert(A)
+
+document.write ("The red fox said to the goat, \"Stop smacking me with yo head!\"<br>\"Eat my horns!\" Replied the goat."
++ "\"Gahhh!!!!!!!\" Screemed the fox.");
+
+var B = "Great " + "Dog"
+document.write(B);
+
+var Family = "The Allards", Dad = "Tim", Mom = "Paige", Son_1 = "Jon", Son_2 = "Morgan";
+document.write(Dad);
+
+document.write(3 + 3)
+
+function My_First_Function() {            // Defining a function and naming it
+    var str = "This is the button text!"; // Defining a variable and giving it a string value 
+    document.getElementById("Button_Text").innerHTML = str; // Putting the value of the variable into the HTML ElementFromPoint with the "Button_Text" id
+}
